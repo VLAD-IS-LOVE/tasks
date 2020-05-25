8a3fcd471fd10c3fc757b68a2fa52a3d99d4c889
VLAD-IS-LOVE <bolshakov.vladislav@gmail.com>
2020-05-25 20:10:47 +0200

    Create JS+HTML_2.js

---
+++ JS+HTML_2.js
@@ -0,0 +1,31 @@
+document.body.style.background = 'black';
+
+let w = 80;
+let h = 60;
+
+createRussianFlag = (width, height) => {
+  let div = document.createElement('div');
+  div.className = "flag-wrapper";
+  document.body.appendChild(div);
+  let totalSymbols = width * height;
+  let symbolsPerColor = totalSymbols / 3;
+
+  for (let color of ['white', 'blue', 'red']) {
+    let divPerColor = document.createElement('div');
+    let flag = document.querySelector('.flag-wrapper');
+    flag.appendChild(divPerColor);
+
+    for (let i = 0; i < symbolsPerColor; i++) {
+      let span = document.createElement('span');
+      let br = document.createElement('br');
+      if (i % width == 0 && i > 0) {
+        divPerColor.appendChild(br);
+      }
+      span.textContent = 'O';
+      span.style.color = color;
+      divPerColor.appendChild(span);
+    }
+  }
+}
+
+createRussianFlag(w, h);
