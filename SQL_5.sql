93811aa1f9708681eff6d7cdb918202d14e94dc3
VLAD-IS-LOVE <bolshakov.vladislav@gmail.com>
2020-05-26 15:51:44 +0200

    Create SQL_5.sql

---
+++ SQL_5.sql
@@ -0,0 +1,7 @@
+Добавить в новый отдел троих новых сотрудников и одного руководителя (с учетом заполнения всех полей)
+
+INSERT INTO EMPLOYEE
+VALUES ('employee_id_1', 'department_id', 'chief_id', 'employee_name_1', 'employee_salary_1'),
+       ('employee_id_2', 'department_id', 'chief_id', 'employee_name_2', 'employee_salary_2'),
+       ('employee_id_3', 'department_id', 'chief_id', 'employee_name_3', 'employee_salary_3'),
+       ('chief_id', 'department_id', 'chief_id', 'chief_name', 'chief_salary')
