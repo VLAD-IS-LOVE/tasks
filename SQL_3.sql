dccedfebfea8aef6f602f09a33b3f16441c24602
VLAD-IS-LOVE <bolshakov.vladislav@gmail.com>
2020-05-26 16:57:00 +0200

    Update SQL_3.sql

--- SQL_3.sql
+++ SQL_3.sql
@@ -1,3 +1,5 @@
+-- Найти список ID отделов с максимальной суммарной зарплатой сотрудников
+
 WITH SUM_SALARY AS ( SELECT DEPARTMENT_ID, SUM(SALARY) SALARY 
                      FROM EMPLOYEE 
                      GROUP BY DEPARTMENT_ID ) 
